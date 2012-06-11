073701f958f24e4b6aa172880d008cd879b45d65
Your Name <jakkula4@gmail.com>
2012-06-11 15:02:14 +0530

    heroku

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,8 @@
+
 class Friend < ActiveRecord::Base
   attr_accessible :AboutME, :DOB, :cell, :email, :name
+  validates_presence_of :cell, :message => "cell number is required."
+  validates_presence_of :name, :message => "......please let mohan know your name...."
+  validates_uniqueness_of :cell, :message => "There is already an account with that email."
+  validates :email, :presence => true, :email => true
 end
